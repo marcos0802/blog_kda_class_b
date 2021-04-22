eb1765e35680d93aac87131bea59377849509daa
root <root@localhost.localdomain>
2021-04-22 15:27:16 +0100

    Okay

---
+++ scripts/index.js
@@ -0,0 +1,37 @@
+window.onload = function () {
+  fetch("http://localhost:3000/apprenants")
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      data.forEach(function (student) {
+        const studentsContainer = document.querySelector(".student-container");
+        studentsContainer.classList = "student-container";
+        const card = document.createElement("div");
+        card.classList = "card";
+
+        const cardContent = document.createElement("div");
+        const cardImage = document.createElement("img");
+        const cardTitle = document.createElement("div");
+        const cardAbout = document.createElement("p");
+        const cardEmail = document.createElement("h5");
+
+        cardContent.classList = "card-content";
+        cardTitle.classList = "card-title";
+
+        cardImage.setAttribute("src", `images/${student.profile}`);
+        cardTitle.textContent = student.names;
+        cardEmail.textContent = `Email: ${student.email}`;
+        cardAbout.textContent = student.about;
+
+        cardContent.appendChild(cardImage);
+        cardContent.appendChild(cardTitle);
+        cardContent.appendChild(cardEmail);
+        cardContent.appendChild(cardAbout);
+
+        card.appendChild(cardContent);
+
+        return studentsContainer.appendChild(card);
+      });
+    });
+};
