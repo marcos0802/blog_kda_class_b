29b8df49064be4013b5f0b586a35670206d47d7c
root <root@localhost.localdomain>
2021-04-22 15:29:12 +0100

    servfer

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,5 @@
 window.onload = function () {
-  fetch("http://localhost:3000/apprenants")
+  fetch("https://my-json-server.typicode.com/marcos0802/blog_kda_class_b/apprenants")
     .then(function (response) {
       return response.json();
     })
